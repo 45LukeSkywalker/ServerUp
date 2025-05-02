61d598efbf1e019ea81b05ae09885e6f7a0a2829
Willem-van-Dorp <176481991+Willem-van-Dorp@users.noreply.github.com>
2025-05-02 17:55:21 +0100

    Logic

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import subprocess
-pinger = subprocess.run(["ping", ])
-pinger.stdout.decode() # Output
+import json
+with open("config.json", "r") as file:
+    config = json.load(file)
+
+serverips = config["servers"]
+
+for ip in serverips:
+    pinger = subprocess.run(["ping", ip], capture_output=True)
+    if "Ping request could not find host" in pinger.stdout.decode():
+        print(f"{ip}: ping failure")
+    else:
+        print(f"{ip}: ping success")
